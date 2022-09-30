978b755f6d60164a09fd6e1b4efc943bf3066ff7
Luca Dei Zotti <luca.deizotti@gmail.com>
2022-09-30 15:44:18 +0200

    [add] drop and redact implementations for fields

--- lib/redact_ex/redactable.ex
+++ lib/redact_ex/redactable.ex
@@ -10,11 +10,38 @@ defprotocol RedactEx.Redactable do
     @derive RedactEx.Redactable(
       fields: [
         myfield1: {MyModule, :redact_function_one},
-        myfield2: {MyModule, :redact_function_two}
+        myfield2: {MyModule, :redact_function_two},
     ])
     defstruct [:myfield1, :myfield2]
   end
   ```
+
+  options
+
+  * `:fields`    possible values of `fields` are a keyword list of configurations, where configuration can be
+    * `{module, function}`, e.g.
+      ``` elixir
+      fields: [
+          myfield1: {MyModule, :redact_function_one},
+          myfield2: {MyModule, :redact_function_two},
+      ]
+      ```
+      In this case redacting will be applied to `myfield1` and `myfield2`  using module.function as configured
+    * `:redact`, e.g.
+      ``` elixir
+      fields: [
+        field: :redact
+      ]
+      ```
+      In this case it is expected for value in `field` to have an Impl for `RedactEx.Redactable`, and `RedactEx.Redactable.redact/1` will be called
+    * `:drop`, e.g.
+      ``` elixir
+      fields: [
+        field: :drop
+      ]
+      ```
+      In this case `:field` key is entirely dropped. **NOTE** that this also means
+      struct information will be lost, and the value will become a map
 
   e.g. suppose you define a module like
 
@@ -33,6 +60,8 @@ defprotocol RedactEx.Redactable do
 
   Good practices could be to implement the Inspect protocol for sensitive data and use the derived `redact`
   capabilities of the inspected module inside its implementation.
+  You can both `derive` single fields in your struct, or define whatever implementation fits best
+  for your structs as `defimpl RedactEx.Redactable, for: MyStruct`
 
   Then you can enforce `inspect`ing structures in your logs or export functions. Unluckily this is not a simple practice
   to enforce in an automatic flavour.
@@ -58,6 +87,30 @@ defimpl RedactEx.Redactable, for: Any do
   defmacro __deriving__(module, struct, opts) do
     fields = fields_to_redact(struct, opts)
 
+    case fields do
+      {redacter_module, function} -> redact_all_ast(module, redacter_module, function)
+      fields when is_list(fields) -> redact_ast(module, fields)
+    end
+  end
+
+  def redact(value) do
+    raise Protocol.UndefinedError,
+      protocol: @protocol,
+      value: value,
+      description: "RedactEx.Redactable protocol must always be explicitly implemented"
+  end
+
+  defp redact_all_ast(module, redacter_module, function) do
+    quote do
+      defimpl RedactEx.Redactable, for: unquote(module) do
+        def redact(%_{} = value) do
+          :erlang.apply(unquote(redacter_module), unquote(function), [value])
+        end
+      end
+    end
+  end
+
+  defp redact_ast(module, fields) do
     quote do
       defimpl RedactEx.Redactable, for: unquote(module) do
         def redact(%_{} = value) do
@@ -65,6 +118,9 @@ defimpl RedactEx.Redactable, for: Any do
             unquote(fields),
             value,
             fn
+              {key, :drop}, acc ->
+                acc |> Map.from_struct() |> Map.drop([key])
+
               {key, :redact}, acc ->
                 Map.update!(acc, key, &RedactEx.Redactable.redact/1)
 
@@ -75,13 +131,6 @@ defimpl RedactEx.Redactable, for: Any do
         end
       end
     end
-  end
-
-  def redact(value) do
-    raise Protocol.UndefinedError,
-      protocol: @protocol,
-      value: value,
-      description: "RedactEx.Redactable protocol must always be explicitly implemented"
   end
 
   defp fields_to_redact(struct, opts) do
@@ -99,12 +148,19 @@ defimpl RedactEx.Redactable, for: Any do
   end
 
   defp check_fields_to_redact_configuration!(fields_to_redact) do
-    Enum.each(fields_to_redact, fn {_, {redacter_module, redacter_function}} ->
-      Code.ensure_loaded!(redacter_module)
-      # true = Module.defines?(redacter_module, {redacter_function, 1})
-      true = Kernel.function_exported?(redacter_module, redacter_function, 1)
+    Enum.each(fields_to_redact, fn
+      {_, action} when action in [:redact, :drop] ->
+        true
+
+      {_, {redacter_module, redacter_function}} ->
+        Code.ensure_loaded!(redacter_module)
+        # true = Module.defines?(redacter_module, {redacter_function, 1})
+        true = Kernel.function_exported?(redacter_module, redacter_function, 1)
     end)
   end
+
+  # redact all fields
+  defp check_fields_to_redact!(_struct_fields, :redact), do: :ok
 
   defp check_fields_to_redact!(struct_fields, fields_to_redact) do
     case keys_from_opts(fields_to_redact) -- struct_fields do
--- test/support/derive/derive_structs.ex
+++ test/support/derive/derive_structs.ex
@@ -1,13 +1,42 @@
 defmodule Support.RedactEx.Derive.Redacter do
-  def redact0(value), do: "#{value} redacted0"
+  def redact0(value) when is_binary(value), do: "#{value} redacted0"
+
   def redact1(value), do: "#{value} redacted1"
+end
+
+defmodule Support.RedactEx.Derive.SubDeriveMe do
+  @derive {RedactEx.Redactable,
+           fields: [
+             field: {Support.RedactEx.Derive.Redacter, :redact0}
+           ]}
+  defstruct [:field]
+end
+
+defmodule Support.RedactEx.Derive.ImplDeriveMeAsString do
+  defstruct [:field0, :field1, :field2]
+end
+
+defimpl RedactEx.Redactable, for: Support.RedactEx.Derive.ImplDeriveMeAsString do
+  def redact(%Support.RedactEx.Derive.ImplDeriveMeAsString{} = _value) do
+    "I was an ImplDeriveMeAsString struct, I became a string"
+  end
 end
 
 defmodule Support.RedactEx.Derive.DeriveMe do
   @derive {RedactEx.Redactable,
            fields: [
              field0: {Support.RedactEx.Derive.Redacter, :redact0},
-             field1: {Support.RedactEx.Derive.Redacter, :redact1}
+             field1: {Support.RedactEx.Derive.Redacter, :redact1},
+             substruct: :redact
            ]}
-  defstruct [:field0, :field1]
+  defstruct [:field0, :field1, :substruct, :keepme, :keepme_number]
+end
+
+defmodule Support.RedactEx.Derive.DropMe do
+  @derive {RedactEx.Redactable,
+           fields: [
+             field0: {Support.RedactEx.Derive.Redacter, :redact0},
+             field_drop: :drop
+           ]}
+  defstruct [:field0, :field_drop]
 end
