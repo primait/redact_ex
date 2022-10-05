1d114140b6f7a47ec49803603ceb401e10616503
Luca Dei Zotti <luca.deizotti@gmail.com>
2022-10-05 15:47:48 +0200

    [add] links to documentation main page

--- lib/redact_ex.ex
+++ lib/redact_ex.ex
@@ -3,9 +3,9 @@ defmodule RedactEx do
   RedactEx provides protocols and derivatoin utilities to work with
   sensitive data that should be redacted in logs and external facing tools
 
-  * `RedactEx.Redacter` module gives you automagical generation of redacting
+  * `[RedactEx.Redacter](./RedactEx.Redacter.html)` module gives you automagical generation of redacting
      functions under a desired module namespace
-  * `RedactEx.Redactable` is the protocol for which implementation and
+  * `[RedactEx.Redactable](./RedactEx.Redactable.html)` is the protocol for which implementation and
      derivation should be created to ensure the best possible practices
      in your codebase
   """
